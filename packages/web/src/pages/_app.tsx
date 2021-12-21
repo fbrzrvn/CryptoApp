54683b10153c3944995e7c7104f10d054c26cdac
fabri <fabrizio@fxstreet.com>
2021-12-21 16:07:17 +0100

    fix: tsconfig, eslint/prettier

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { GlobalStyles } from '@/styles/globals';
+import '@/styles/globals.ts';
 import type { AppProps } from 'next/app';
 import React from 'react';
-import '../styles/globals.ts';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
