714cb16353281fb7816bd46f6cb8c1c329fba3a3
fabri <fabrizio@fxstreet.com>
2021-12-28 09:00:21 +0100

    fix: 404 page fetch data from server

--- packages/client/src/pages/404.tsx
+++ packages/client/src/pages/404.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const NotFound = () => {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('http://localhost:4000/*')
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div>
-      <h1>Page not found</h1>
+      <h1>{!data ? 'Loading...' : data}</h1>
     </div>
   );
 };
