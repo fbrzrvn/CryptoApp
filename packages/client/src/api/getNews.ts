64c72ac36af3b7d760fabc1aebca8df9b90a6ee2
Fabrizio Ervini <fabri.es018@gmail.com>
2022-01-06 12:39:05 +0100

    feat: add category props and return sorted news

--- packages/client/src/api/getNews.ts
+++ packages/client/src/api/getNews.ts
@@ -2,14 +2,21 @@ import mapNews from 'mappers/news';
 import { News, NewsMapped } from 'Models/News';
 import makeApiCall from './api';
 
-const getNews = async (): Promise<NewsMapped[]> => {
-  const endpoint = `${process.env.REACT_APP_RAPID_API_NEWS_API_URL}/search?q=cryptos&safeSearch=Off&textFormat=Raw&freshness=Day&count=20`;
+const getNews = async (
+  category: string = 'cryptocurrency',
+): Promise<NewsMapped[]> => {
+  const endpoint = `${process.env.REACT_APP_RAPID_API_NEWS_API_URL}/search?q=${category}&safeSearch=Off&textFormat=Raw&freshness=Day&count=20`;
   const { value } = await makeApiCall(endpoint, 'GET', {
     'x-bingapis-sdk': 'true',
     'x-rapidapi-key': process.env.REACT_APP_RAPID_API_NEWS_API_KEY,
     'x-rapidapi-host': process.env.REACT_APP_RAPID_API_HOST,
   });
-  return value.map((news: News) => mapNews(news));
+
+  const sortedData: News[] = value.sort((a: News, b: News) =>
+    a.datePublished > b.datePublished ? -1 : 1,
+  );
+
+  return sortedData.map((news: News) => mapNews(news));
 };
 
 export default getNews;
