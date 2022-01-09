e19d3558a5e592acd7a290f027d1e5a9d2b75909
Fabrizio Ervini <fabri.es018@gmail.com>
2022-01-09 14:16:50 +0100

    refactor: tags constant and styles

--- packages/client/src/components/NewsTags/styles.ts
+++ packages/client/src/components/NewsTags/styles.ts
@@ -2,27 +2,36 @@ import styled from 'styled-components';
 import { COLORS } from 'styles/Colors';
 
 export const TagsWrapper = styled.div`
+  position: sticky;
+  top: 0;
   display: flex;
   margin-top: 1rem;
+  padding: 1rem 0;
+  background: ${COLORS.gris100};
   scroll-snap-type: x mandatory;
   scroll-behavior: smooth;
   overflow-x: scroll;
   cursor: pointer;
+  @media screen and (min-width: 992px) {
+    top: -1.5rem;
+  }
 `;
 export const Tag = styled.span<{ isActive: boolean }>`
   flex-shrink: 0;
   margin-right: 0.5rem;
   padding: 0.25rem 0.5rem;
-  background: ${({ isActive }) => (isActive ? `${COLORS.blue50}` : '')};
-  border-radius: 4px;
   color: ${({ isActive }) =>
     isActive ? `${COLORS.blue500}` : `${COLORS.gris900}`};
-  font-family: 'Montserrat', sans-serif;
+  border-bottom: ${({ isActive }) =>
+    isActive ? `3px solid ${COLORS.blue500}` : '3px solid transparent'};
   font-weight: 500;
-  font-size: 1rem;
+  font-size: 1.1rem;
   line-height: 1.1;
   letter-spacing: 1px;
-  scroll-snap-align: start;
+  scroll-snap-align: center;
+  &:last-of-type {
+    margin-right: 0;
+  }
   &:hover {
     color: ${COLORS.blue500};
   }
