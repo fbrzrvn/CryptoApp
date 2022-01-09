cb2f45880a58c7dccb0fc30a7f00427ea4178542
Fabrizio Ervini <fabri.es018@gmail.com>
2022-01-09 13:04:58 +0100

    refactor: update dotenv vars add .env.example

--- packages/client/src/api/getNews.ts
+++ packages/client/src/api/getNews.ts
@@ -5,11 +5,11 @@ import makeApiCall from './api';
 const getNews = async (
   category: string = 'cryptocurrency',
 ): Promise<NewsMapped[]> => {
-  const endpoint = `${process.env.REACT_APP_RAPID_API_NEWS_API_URL}/search?q=${category}&safeSearch=Off&textFormat=Raw&freshness=Day&count=20`;
+  const endpoint = `${process.env.REACT_APP_BING_NEWS_API_URL}/search?q=${category}&safeSearch=Off&textFormat=Raw&freshness=Day&count=20`;
   const { value } = await makeApiCall(endpoint, 'GET', {
     'x-bingapis-sdk': 'true',
-    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_NEWS_API_KEY,
-    'x-rapidapi-host': process.env.REACT_APP_RAPID_API_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+    'x-rapidapi-host': process.env.REACT_APP_BING_NEWS_HOST,
   });
 
   const sortedData: News[] = value.sort((a: News, b: News) =>
