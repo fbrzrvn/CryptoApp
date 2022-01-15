16669e650561b901f17984ec0dc09896bd9cea56
Fabrizio Ervini <fabri.es018@gmail.com>
2022-01-15 12:30:09 +0100

    refactor: migrate from chart.js to apexchart

--- packages/client/src/components/LineChart/index.tsx
+++ packages/client/src/components/LineChart/index.tsx
@@ -1,69 +1,130 @@
-import {
-  CategoryScale,
-  Chart as ChartJS,
-  Legend,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
-} from 'chart.js';
-import { CryptoHistory } from 'models/Crypto';
+import getCryptoHistory from 'api/getCryptoHistory';
+import { Loader, LoadError } from 'components';
+import { CurrencyContext } from 'context/Currency';
+import { CryptoHistoryResponseData } from 'models/Crypto';
 import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { COLORS } from 'styles/Colors';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-);
+import Chart from 'react-apexcharts';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import { ChartWrapper, Wrapper } from './styles';
 
 interface LineChartProps {
-  data: CryptoHistory[];
-  timePeriod: string;
-  setTimePeriod: (timePeriod: string) => void;
+  cryptoPrice: number;
 }
 
-const LineChart: React.VFC<LineChartProps> = ({ data }) => {
+const LineChart: React.VFC<LineChartProps> = ({ cryptoPrice }) => {
+  const { currency } = React.useContext(CurrencyContext);
+  const [timePeriod, setTimePeriod] = React.useState('24h');
+
+  const { id } = useParams();
+
+  const { data, error, isError, isLoading } = useQuery<
+    CryptoHistoryResponseData,
+    Error
+  >(
+    ['cryptoHistory', id, timePeriod, currency],
+    () => getCryptoHistory(id, timePeriod, currency),
+    {
+      staleTime: Infinity,
+    },
+  );
+
   const price = [];
   const timestamp = [];
 
-  for (let i = 0; i < data.length; i += 1) {
-    price.push(data[i].price);
-    timestamp.push(new Date(data[i].timestamp * 1000).toLocaleDateString());
+  if (data) {
+    for (let i = 0; i < data.history.length; i += 1) {
+      price.push(data.history[i].price.slice(0, 3));
+      timestamp.push(
+        new Date(data.history[i].timestamp * 1000).toLocaleDateString(),
+      );
+    }
   }
 
-  const charData = {
-    labels: timestamp,
-    datasets: [
-      {
-        label: 'Price in EUR',
-        data: price,
-        fill: false,
-        backgroundColor: COLORS.gris100,
-        borderColor: COLORS.blue500,
-        tension: 0.1,
-      },
-    ],
+  const chartData = {
     options: {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
+      chart: {
+        id: 'area-chart',
+        height: 350,
+        zoom: {
+          autoScaleYaxis: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      markers: {
+        size: 0,
+        style: 'hollow',
+      },
+      xaxis: {
+        categories: timestamp,
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          stops: [0, 90, 100],
+        },
+      },
+      stroke: {
+        curve: 'smooth' as 'smooth',
+        width: 2,
       },
     },
+    series: [
+      {
+        name: 'Price in EUR',
+        data: price,
+      },
+    ],
   };
 
-  return <Line data={charData} />;
+  return (
+    <>
+      <Wrapper>
+        <div>
+          <p>Price to EUR</p>
+          <p>{cryptoPrice}</p>
+        </div>
+        <div>
+          <p>{timePeriod} change</p>
+          <p>
+            {+data?.change > 0 ? '+' : '-'}
+            {data?.change}%
+          </p>
+        </div>
+
+        <select
+          defaultValue={timePeriod}
+          onChange={(e) => setTimePeriod(e.target.value)}
+        >
+          <option value="3h">3h</option>
+          <option value="24h">24h</option>
+          <option value="7d">7d</option>
+          <option value="30d">30d</option>
+          <option value="3m">3m</option>
+          <option value="1y">1y</option>
+          <option value="3y">3y</option>
+          <option value="5y">5y</option>
+        </select>
+      </Wrapper>
+      <ChartWrapper>
+        {isLoading && <Loader />}
+        {isError && <LoadError error={error.message} />}
+        {data && (
+          <Chart
+            options={chartData.options}
+            series={chartData.series}
+            type="area"
+            width="100%"
+          />
+        )}
+      </ChartWrapper>
+    </>
+  );
 };
 
 export default LineChart;
