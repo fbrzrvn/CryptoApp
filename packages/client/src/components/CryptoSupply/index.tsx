8cc358bdecfb7cad9ce0020960592c80dad4c91a
Fabrizio Ervini <fabri.es018@gmail.com>
2022-01-18 21:24:29 +0100

    feat: add basic cryptoSupply component

---
+++ packages/client/src/components/CryptoSupply/index.tsx
@@ -0,0 +1,104 @@
+import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
+import {
+  faCheckCircle,
+  faExclamationCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Tooltip from 'components/Tooltip';
+import { millify } from 'millify';
+import { CryptoMapped } from 'models/Crypto';
+import React from 'react';
+import {
+  Container,
+  Overview,
+  OverviewInfo,
+  OverviewInfoInner,
+  OverviewPercentage,
+  OverviewSupplyDetails,
+  OverviewSupplyVerified,
+  Percentage,
+  Wrapper,
+  WrapperInner,
+} from './styles';
+
+const CryptoSupply: React.VFC<CryptoMapped> = (crypto) => {
+  const data = [
+    {
+      id: 11,
+      title: 'Circulating supply',
+      value: millify(crypto.supplyCirculating),
+      tooltipText:
+        'Circulating supply is the total amount of tokens in circulation, including unsold tokens.',
+    },
+    {
+      id: 12,
+      title: 'Total supply',
+      value: millify(crypto.supplyTotal),
+      tooltipText:
+        'Total supply is the total amount of tokens in existence, including unsold tokens.',
+    },
+    // {
+    //   id: 13,
+    //   title: 'Max supply',
+    //   value: millify(234000000),
+    //   tooltipText:
+    //     'Max supply is the maximum amount of tokens that can ever exist.',
+    // },
+    // {
+    //   id: 14,
+    //   title: 'Issuance blockchain',
+    //   value: 'Ethereum',
+    //   tooltipText:
+    //     'Issuance blockchain is the blockchain that issued the tokens.',
+    // },
+  ];
+
+  const PercentageInfo = {
+    id: 11,
+    tooltipText:
+      'This is the total versus the circulating supply. It shows the percentage of the total supply that is circulating.',
+  };
+
+  return (
+    <Container>
+      <Overview>
+        <OverviewPercentage>
+          <Percentage>
+            {Math.round((crypto.supplyTotal / crypto.supplyCirculating) * 100)}%
+          </Percentage>
+          <Tooltip item={PercentageInfo} />
+        </OverviewPercentage>
+        <OverviewInfo>
+          <OverviewInfoInner>
+            <OverviewSupplyVerified
+              href="https://support.coinranking.com/article/76-what-does-verified-supply-mean"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={
+                  crypto.supplyCorfirmed ? faCheckCircle : faExclamationCircle
+                }
+              />
+              {crypto.supplyCorfirmed ? 'Verified supply' : 'Unverified supply'}
+            </OverviewSupplyVerified>
+            <OverviewSupplyDetails>
+              <FontAwesomeIcon icon={faQuestionCircle} /> Details
+            </OverviewSupplyDetails>
+          </OverviewInfoInner>
+        </OverviewInfo>
+      </Overview>
+      {data.map((item) => (
+        <Wrapper key={item.id}>
+          <WrapperInner>
+            <span>{item.title}</span>
+            <Tooltip item={item} />
+          </WrapperInner>
+          <p>{item.value}</p>
+        </Wrapper>
+      ))}
+    </Container>
+  );
+};
+
+export default CryptoSupply;
---
+++ packages/client/src/components/CryptoSupply/styles.ts
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+import { COLORS } from 'styles/Colors';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  background-color: ${COLORS.blue50};
+  background-color: ${COLORS.lightBlue50};
+  border-radius: 8px;
+`;
+export const Overview = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+  width: 100%;
+  padding: 1.5rem 3rem 2rem;
+  @media (min-width: 680px) {
+    flex-direction: row-reverse;
+  }
+  @media screen and (min-width: 1280px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
+export const OverviewPercentage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-color: transparent;
+  border: 7px solid ${COLORS.blue700};
+`;
+export const Percentage = styled.p`
+  font-weight: 700;
+  font-size: 1.2rem;
+`;
+export const OverviewInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const OverviewInfoInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  @media (min-width: 680px) {
+    flex-direction: row;
+  }
+  @media screen and (min-width: 1280px) {
+    flex-direction: column;
+    margin-bottom: 1rem;
+  }
+`;
+export const OverviewSupplyVerified = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: ${COLORS.success500};
+  cursor: pointer;
+  transition: all 0.25s ease-in-out;
+  &:hover {
+    color: ${COLORS.success700};
+    text-decoration: none;
+  }
+`;
+export const OverviewSupplyDetails = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: ${COLORS.blue900};
+  font-weight: 700;
+  cursor: default;
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+export const OverViewLastUpdate = styled.small`
+  margin-top: 1.5rem;
+  color: ${COLORS.blue900};
+  font-size: 0.8rem;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  aling-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 1rem 2rem;
+  border-top: 1px solid ${COLORS.gris400};
+  border-top: 1px solid ${COLORS.lightBlue100};
+  user-select: none;
+  @media screen and (min-width: 1280px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
+export const WrapperInner = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
