370f21ca66ca9b777f898b3ecb2b1b7deb1e5796
Fabrizio Ervini <fabri.es018@gmail.com>
2022-01-18 21:25:16 +0100

    refactor: cryptoDetails

--- packages/client/src/components/CryptoDetails/index.tsx
+++ packages/client/src/components/CryptoDetails/index.tsx
@@ -1,57 +1,48 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import CryptoTable from 'components/CryptoTable';
+import { AreaChart } from 'components/Charts';
+import CryptoStats from 'components/CryptoStats';
+import CryptoSupply from 'components/CryptoSupply';
 import { currencies } from 'constants/currencies';
 import { iconsLinks } from 'constants/iconsLinks';
 import { CurrencyContext } from 'context/Currency';
-import millify from 'millify';
-import { CryptoLinks, CryptoMapped, CryptoTableData } from 'models/Crypto';
+import { CryptoLinks, CryptoMapped } from 'models/Crypto';
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
-import { formatCurrency, getCurrencySymbol } from 'utils';
 import {
   CryptoDescription,
+  CryptoFlexLayout,
   CryptoHeader,
+  CryptoIconLink,
   CryptoImage,
   CryptoLabel,
-  CryptoLink,
   CryptoLinksWrapper,
+  CryptoNameLink,
   CryptoSection,
   CryptoSubtitle,
   CryptoTitle,
+  CryptoTypeLink,
 } from './styles';
 
-const CryptoDetails: React.VFC<CryptoMapped> = (crypto) => {
+interface CryptoDetailsProps {
+  crypto: CryptoMapped;
+}
+
+const CryptoDetails: React.VFC<CryptoDetailsProps> = ({ crypto }) => {
   const { currency } = React.useContext(CurrencyContext);
+  const [timePeriod, setTimePeriod] = React.useState('24h');
+
   const currentCurrency = currencies.find((c) => c.uuid === currency);
 
-  const valueStats: CryptoTableData[] = [
-    {
-      title: 'Price',
-      value: formatCurrency(crypto.price, getCurrencySymbol(currency)),
-    },
-    { title: '24h Volume', value: millify(crypto['24hVolume']) },
-    { title: 'Market Cap', value: millify(crypto.marketCap) },
-    {
-      title: 'All Time High',
-      value: formatCurrency(
-        +crypto.allTimeHigh.price,
-        getCurrencySymbol(currency),
-      ),
-    },
-  ];
-
-  const infoStats: CryptoTableData[] = [
-    { title: 'Total Supply', value: millify(crypto.supplyTotal) },
-    { title: 'Circulating Supply', value: millify(crypto.supplyCirculating) },
-    { title: 'N. Markets', value: crypto.numberOfMarkets },
-    { title: 'N. Exchanges', value: crypto.numberOfExchanges },
-  ];
+  const handleClick = (url: string): void => {
+    window.open(url, '_blank', 'noopener noreferrer');
+  };
 
   return (
     <article>
       <CryptoHeader>
         <CryptoImage src={crypto.iconUrl} alt={crypto.name} />
-        <h1>{crypto.name}</h1>
+        <CryptoTitle>
+          {crypto.name} ({crypto.symbol})
+        </CryptoTitle>
       </CryptoHeader>
       <CryptoSubtitle>
         {`${crypto.name} live price in ${currentCurrency.name}
@@ -59,14 +50,32 @@ const CryptoDetails: React.VFC<CryptoMapped> = (crypto) => {
       </CryptoSubtitle>
 
       <CryptoSection>
-        <CryptoLabel>Value Statistics</CryptoLabel>
-        <CryptoTable tableData={valueStats} />
+        <AreaChart
+          cryptoPrice={crypto.price}
+          timePeriod={timePeriod}
+          setTimePeriod={setTimePeriod}
+        />
       </CryptoSection>
 
-      <CryptoSection>
-        <CryptoLabel>Info Statistics</CryptoLabel>
-        <CryptoTable tableData={infoStats} />
-      </CryptoSection>
+      <CryptoFlexLayout>
+        <CryptoSection>
+          <CryptoLabel>{crypto.symbol} value statistics</CryptoLabel>
+          <CryptoSubtitle>
+            An overview showing the statistics of {crypto.name}, such as the
+            base and quote currency, the rank, and trading volume.
+          </CryptoSubtitle>
+          <CryptoStats {...crypto} />
+        </CryptoSection>
+
+        <CryptoSection>
+          <CryptoLabel>Supply information</CryptoLabel>
+          <CryptoSubtitle>
+            View the total and circulating supply of {crypto.name}, including
+            details on how the supplies are calculated.
+          </CryptoSubtitle>
+          <CryptoSupply {...crypto} />
+        </CryptoSection>
+      </CryptoFlexLayout>
 
       <CryptoDescription>
         <CryptoTitle>What is {crypto.name}?</CryptoTitle>
@@ -74,20 +83,14 @@ const CryptoDetails: React.VFC<CryptoMapped> = (crypto) => {
       </CryptoDescription>
 
       <CryptoSection>
-        <h3>{crypto.name} Links</h3>
-        <CryptoLinksWrapper>
-          {crypto.links.map((link: CryptoLinks, i) => (
-            <CryptoLink
-              key={i}
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={iconsLinks[link.type]} />
-              {link.type}
-            </CryptoLink>
-          ))}
-        </CryptoLinksWrapper>
+        <CryptoLabel>{crypto.name} links</CryptoLabel>
+        {crypto.links.map((link: CryptoLinks, i) => (
+          <CryptoLinksWrapper key={i} onClick={() => handleClick(link.url)}>
+            <CryptoIconLink icon={iconsLinks[link.type]} />
+            <CryptoTypeLink>{link.type}</CryptoTypeLink>
+            <CryptoNameLink>{link.name}</CryptoNameLink>
+          </CryptoLinksWrapper>
+        ))}
       </CryptoSection>
     </article>
   );
--- packages/client/src/components/CryptoDetails/styles.ts
+++ packages/client/src/components/CryptoDetails/styles.ts
@@ -1,15 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 import { COLORS } from 'styles/Colors';
 
-export const CryptoSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  margin: 1rem 0;
-`;
 export const CryptoHeader = styled.div`
   display: flex;
   align-items: center;
   height: 4rem;
+  margin-bottom: 1rem;
 `;
 export const CryptoImage = styled.img`
   height: 50px;
@@ -18,23 +15,38 @@ export const CryptoImage = styled.img`
   border-radius: 50%;
 `;
 export const CryptoTitle = styled.h2`
-  font-size: 1.5rem;
+  font-size: 2rem;
 `;
 export const CryptoSubtitle = styled.p`
-  font-size: 1.15rem;
+  font-size: 1.5rem;
   font-weight: 300;
+  margin-bottom: 1rem;
+`;
+export const CryptoFlexLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 1rem;
+  @media screen and (min-width: 1280px) {
+    flex-direction: row;
+  }
+`;
+export const CryptoSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin: 1rem 0 2rem;
 `;
 export const CryptoLabel = styled.h3`
-  margin-bottom: 1rem;
+  margin-bottom: 1.25rem;
   color: ${COLORS.gris800};
   font-style: italic;
-  font-size: 1rem;
+  font-size: 1.2rem;
   letter-spacing: 0.02em;
   text-transform: uppercase;
   border-bottom: 1px solid ${COLORS.gris400};
 `;
 export const CryptoDescription = styled.div`
-  margin: 1rem 0;
+  margin: 1rem 0 2rem;
   & p {
     font-size: 1.1rem;
     font-weight: 300;
@@ -42,19 +54,33 @@ export const CryptoDescription = styled.div`
   }
 `;
 export const CryptoLinksWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
-  gap: 1rem;
-  margin-top: 1rem;
-`;
-export const CryptoLink = styled.a`
   display: flex;
-  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  text-transform: capitalize;
-  .svg-inline--fa {
-    width: 2rem;
-    height: 1.5rem;
+  justify-content: space-between;
+  gap: 1rem;
+  padding: 1rem 0;
+  border-bottom: 1px solid ${COLORS.blue50};
+  cursor: pointer;
+  transition: all 0.25s ease-in-out;
+  &:hover {
+    box-shadow: 0px 1px 3px ${COLORS.blue50};
   }
+  @media screen and (min-width: 768px) {
+    padding: 1rem;
+  }
+`;
+export const CryptoIconLink = styled(FontAwesomeIcon)`
+  color: ${COLORS.blue700};
+  font-size: 1.2rem;
+`;
+export const CryptoTypeLink = styled.p`
+  flex: 1;
+  color: ${COLORS.black};
+  font-weight: 700;
+  font-size: 1rem;
+  text-transform: capitalize;
+`;
+export const CryptoNameLink = styled.p`
+  color: ${COLORS.black};
+  font-size: 1rem;
 `;
